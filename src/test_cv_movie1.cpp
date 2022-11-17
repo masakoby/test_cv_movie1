66858f7749cf65bdcef0335e3e96638023c93e40
masakoby <masakoby@gmail.com>
2022-11-17 15:30:10 +0900

    modify

--- src/test_cv_movie1.cpp
+++ src/test_cv_movie1.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+int main(int argc, char* argv[])
+{
+	//std::string path1 = argv[1];
+
+	cv::VideoCapture cap(0);
+
+	cv::Mat mat1;
+	cap >> mat1;
+
+
+	std::string wname = "movie";
+	cv::namedWindow(wname, cv::WINDOW_NORMAL);
+
+	while (1) {
+		cap >> mat1;
+
+		cv::imshow(wname, mat1);
+		int rtn = cv::waitKeyEx(5);
+
+		//double msec1 = cap.get(cv::CAP_PROP_POS_MSEC);
+		//if (msec1 > msec1_end)
+		//	break;
+
+
+	}
+	return 0;
+}
+
